88c002f802a50b89a98afc6f1933b2b5b3635147
zeritte <zeritte@gmail.com>
2021-08-23 00:56:29 +0100

    add in chrome options for headless

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.options import Options
 import datetime
 from time import sleep
+
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--headless")
